90310616d8be54e74b9f3879e2e8649d76d3c18f
catalan <matheuscatalan@outlook.com>
2024-09-04 19:06:06 -0300

    sei la

--- main/FirebaseManager.h
+++ main/FirebaseManager.h
@@ -1,4 +1,14 @@
 #ifndef FIREBASE_MANAGER_H
 #define FIREBASE_MANAGER_H
+#include "ConfigManager.h"
+#include "SensorsManager.h"
 
+/*String getCurrentTimestamp();
+void connectToFirebase(Config &config);
+void sendData(Config &config);
+String processFloats(String data);
+void checkForConfigUpdates(Config &config);
+String getFirebaseConnectionStatus();
+String getCurrentTimestamp(long timezoneOffset);
+*/
 #endif
